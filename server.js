499f0b4cdc9227509d8ed93378f598a65132513d
thearatoek <thearatoek@ibank.com.kh>
2026-02-22 20:55:43 +0700

    add

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 const db = require("./app/models");
 const Role = db.role;
 
-// 🔥 FIXED MongoDB connection
+mongoose.set('strictQuery', true);
+
 db.mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
